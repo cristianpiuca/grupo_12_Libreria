5b98267e1f3867cad9027b01fdd4497b79deef7f
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-05-03 15:44:20 -0300

    cambios

--- Site_Libreria_G12/controllers/usersControllers.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    register : (req,res) => res.render('register'),
-    login : (req,res) => res.render('login')
-}
--- Site_Libreria_G12/routes/users.js
+++ Site_Libreria_G12/routes/users.js
@@ -2,16 +2,8 @@ var express = require('express');
 var router = express.Router();
 const {login, register} = require('../controllers/userController')
 
-<<<<<<< HEAD
-const {register,login} = require('../controllers/usersControllers');
-
 /* /users*/
 router.get('/register', register);
 router.get('/login', login)
-=======
-/* /users */
-router.get('/register', register);
-router.get('/login', login);
->>>>>>> 30d7efa7045a707582b8c15cfc09f1eca4bd2579
 
 module.exports = router;
