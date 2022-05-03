3648a013e93535ae630251135a33d3ffdb618707
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-05-03 15:38:46 -0300

    Merge branch 'develop' into cristian-register

--- Site_Libreria_G12/app.js
+++ Site_Libreria_G12/app.js
@@ -4,26 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-/* app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); */
-
-app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html')))
-app.get('/product-detail',(req,res) => res.sendFile(path.resolve(__dirname,'views','productDetail.html')))
-app.get('/product-cart',(req,res) => res.sendFile(path.resolve(__dirname,'views','productCart.html')))
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
-app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
