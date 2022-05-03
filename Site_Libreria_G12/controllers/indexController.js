9142991453e6254aa88977f5120dbd1e5dafb28c
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2022-05-03 16:22:20 -0300

    login register header styles changes

--- Site_Libreria_G12/controllers/indexController.js
+++ Site_Libreria_G12/controllers/indexController.js
@@ -1,3 +1,5 @@
 module.exports = {
-    index : (req, res) => res.render('index', { title: 'Express' })
+    index : (req, res) => res.render('index', { title: 'Express' }),
+    login :(req, res) => res.render('login'),
+    register :(req, res) => res.render('register')
 }
