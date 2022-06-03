53eb0b93da14bcc144e3d8b5e310c0b69533769b
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-06-03 12:53:56 -0300

    ajustes vista profiel, agregado vista profileEdit

--- Site_Libreria_G12/src/controllers/userController.js
+++ Site_Libreria_G12/src/controllers/userController.js
@@ -2,5 +2,6 @@ module.exports = {
     register : (req, res) => res.render('register'),
     login : (req, res) => res.render('login'),
     password: (req, res) => res.render('password'),
-    profile: (req, res) => res.render('profile')
+    profile: (req, res) => res.render('profile'),
+    profileEdit: (req,res) => res.render('profileEdit')
 }
--- Site_Libreria_G12/src/routes/users.js
+++ Site_Libreria_G12/src/routes/users.js
@@ -1,11 +1,12 @@
 var express = require('express');
 const { route } = require('.');
 var router = express.Router();
-const {login, register, password, profile} = require('../controllers/userController')
+const {login, register, password, profile, profileEdit, updateUser} = require('../controllers/userController')
 
 /* /users*/
 router.get('/register', register);
 router.get('/login', login);
 router.get('/password', password);
 router.get('/profile', profile);
+router.get('/profileEdit', profileEdit)
 module.exports = router;
