c2d10f25ace06ff705cc8877aec82c5c74e8b5e2
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-06-03 09:58:46 -0300

    vista users

--- Site_Libreria_G12/src/controllers/userController.js
+++ Site_Libreria_G12/src/controllers/userController.js
@@ -1,5 +1,6 @@
 module.exports = {
     register : (req, res) => res.render('register'),
     login : (req, res) => res.render('login'),
-    password: (req, res) => res.render('password')
+    password: (req, res) => res.render('password'),
+    profile: (req, res) => res.render('profile')
 }
